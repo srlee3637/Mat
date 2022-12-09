9ffbb34a0f3edc3ec33c5c6c00685aa65296b019
jm <mmmobl@naver.com>
2022-12-09 14:25:07 +0900

    221209_1 jm

---
+++ Mat/src/main/java/com/mat/util/JDBCUtil.java
@@ -0,0 +1,22 @@
+package com.mat.util;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class JDBCUtil {
+
+	public static void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
+
+		try {
+			if(conn != null) conn.close();
+			if(pstmt != null) pstmt.close();
+			if(rs != null) rs.close();
+
+		} catch (Exception e) {
+			System.out.println("Close error!");
+		}
+
+	}
+
+}
